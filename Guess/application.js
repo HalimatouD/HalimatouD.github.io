ebde05d7827a147c4e056ab4ff9bd21b2d1a0e30
HalimatouD <hdiallo@kippnyccp.org>
2017-06-02 13:35:56 -0400

    Update application.js

--- Guess/application.js
+++ Guess/application.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
 	//create click event for submit button
-	$("Submit").click(function(){
-		$("#Submit").css("color", "red");
+	$("#Submit").click(function(){
+		var guess = $("input[name=Box]").val();
+		$("#response").append("You guessed: " + guess);
 	})
 
 
