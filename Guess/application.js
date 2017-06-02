01b6ec6bc7765c0d4eb655240ca319f074b7614c
HalimatouD <hdiallo@kippnyccp.org>
2017-06-02 13:15:02 -0400

    Update application.js

--- Guess/application.js
+++ Guess/application.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
 	//create click event for submit button
-
+	$("Submit").click(function(){
+		$("#Submit").css("color", "red");
+	})
 
 
 	//create click event for clear button
